8529196c007f5acf6a730832ce5d48bb07e61c39
Joseph Romero <joeromero47@gmail.com>
2020-04-19 00:41:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(x)
     end
     puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep (n)
+  n = 10
+  while n > 10
+    puts "#{n} SECOND(S)!"
+    n -= 1
+    sleep 1
+  end
+  puts "HAPPY NEW YEAR!"
+end
