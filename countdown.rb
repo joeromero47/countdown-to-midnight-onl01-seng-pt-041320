f174dfdc364d324373a4735d29ff75070d3fe164
Joseph Romero <joeromero47@gmail.com>
2020-04-19 00:27:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown
-  new_years = 10
-    while new_years > 0
-    puts "#{new_years} SECOND(S)!"
-    new_years += 1
+  x = 10
+    while x > 0
+    puts "#{x} SECOND(S)!"
+    x += 1
     sleep 1
   end
   puts "HAPPY NEW YEAR!"
